4ace31604a6154f6a18c08db3a00721aa74b9fae
某亚瑟 <mouyase@qq.com>
2019-08-02 22:30:09 +0800

    Update Code

--- src/main/java/tech/yojigen/boorutagscn/Main.java
+++ src/main/java/tech/yojigen/boorutagscn/Main.java
@@ -4,19 +4,13 @@ import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 import com.google.gson.reflect.TypeToken;
 
-import org.jsoup.Jsoup;
-import org.jsoup.nodes.Document;
-import org.jsoup.nodes.Element;
+import org.apache.commons.text.StringEscapeUtils;
 
 import java.io.IOException;
-import java.lang.reflect.Parameter;
 import java.lang.reflect.Type;
-import java.net.URL;
-import java.net.URLEncoder;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.LinkOption;
-import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.Base64;
 import java.util.HashMap;
@@ -33,25 +27,43 @@ import okhttp3.Response;
 public class Main {
 
     public static void main(String[] args) throws IOException {
-        Pattern pattern = Pattern.compile("^((?!\\|)[0-9a-zA-Z\\u0000-\\u00FF])+$");
+        Pattern tagsPattern = Pattern.compile("^((?!\\|)[0-9a-zA-Z\\u0000-\\u00FF])+$");
         Gson gson = new GsonBuilder().disableHtmlEscaping().create();
         OkHttpClient client = new OkHttpClient.Builder().connectTimeout(5, TimeUnit.MINUTES).readTimeout(10, TimeUnit.MINUTES).build();
-        System.out.println("获取数据");
+        System.out.println("获取Tag数据");
         Request request = new Request.Builder().url("https://yande.re/tag.json?limit=0&order=name").build();
         Response response = client.newCall(request).execute();
         String jsonTags = response.body().string();
-        System.out.println("解析数据");
+        System.out.println("解析Tag数据");
         Type type = new TypeToken<List<TagBean>>() {
         }.getType();
         List<TagBean> tags = gson.fromJson(jsonTags, type);
+        System.out.println("获取翻译数据");
+        request = new Request.Builder().url("https://docs.google.com/document/d/e/2PACX-1vQSeSI4PU70eBoonfjiNpzQY66VqpwZoJDW4fUS8piXTTpQe57LKDb1KRZnRZEOtwH9Cvhq1dW2hLVp/pub").build();
+        response = client.newCall(request).execute();
+        String translateText = StringEscapeUtils.unescapeHtml4(response.body().string());
+//        System.out.println(translateText);
+        Pattern translatePattern = Pattern.compile("<p class=\"c1\"><span class=\"c0\">(.+?)\\|(.+?)<\\/span><\\/p>");
+        Matcher translateMatcher = translatePattern.matcher(translateText);
+        System.out.println("解析翻译数据");
+        Map<String, String> translateMap = new HashMap<>();
+        while (translateMatcher.find()) {
+            translateMap.put(translateMatcher.group(1), translateMatcher.group(2));
+        }
         if (!Files.exists(Paths.get("./tags"), LinkOption.NOFOLLOW_LINKS)) {
             Files.createDirectory(Paths.get("./tags"));
         }
+        System.out.println("写入翻译数据");
         for (TagBean tag : tags) {
-            Matcher matcher = pattern.matcher(tag.getName());
+            Matcher matcher = tagsPattern.matcher(tag.getName());
             if (matcher.find()) {
-                Files.write(Paths.get("./tags/" + Base64.getUrlEncoder().encodeToString(tag.getName().getBytes(StandardCharsets.UTF_8))), tag.getName().getBytes(StandardCharsets.UTF_8));
+                if (translateMap.containsKey(tag.getName())) {
+                    Files.write(Paths.get("./tags/" + Base64.getUrlEncoder().encodeToString(tag.getName().getBytes(StandardCharsets.UTF_8))), translateMap.get(tag.getName()).getBytes(StandardCharsets.UTF_8));
+                } else {
+                    Files.write(Paths.get("./tags/" + Base64.getUrlEncoder().encodeToString(tag.getName().getBytes(StandardCharsets.UTF_8))), tag.getName().getBytes(StandardCharsets.UTF_8));
+                }
             }
         }
+        System.out.println("数据写入完成");
     }
 }
